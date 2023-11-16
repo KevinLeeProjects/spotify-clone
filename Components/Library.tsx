b1c9f77449d28955114fdbdae97e2b3bfdbd95d5
Kevin <kevinlee.3191@gmail.com>
2023-11-16 15:24:18 -0800

    inital commit

---
+++ Components/Library.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {TbPlaylist} from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
+const Library = () => {
+    const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
+    const { user } = useUser();
+
+    const onClick = () => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+
+        return uploadModal.onOpen();
+    }
+
+    return (
+        <div className="flex flex-col">
+            <div className="
+                flex
+                items-center 
+                justify-between 
+                px-5 
+                pt-4">
+                <div className="
+                    inline-flex
+                    items-center
+                    gap-x-2
+                ">
+                    <TbPlaylist size={26} className="text-neutral-400"/>
+                    <p
+                        className="
+                            text-neutral-400
+                            font-medium
+                            text-md
+                        "
+                    >
+                        Your Library
+                    </p>
+                </div>
+                <AiOutlinePlus 
+                    onClick={onClick}
+                    size={20}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            ">
+                List of Songs!
+            </div>
+        </div>
+    )
+}
+
+export default Library;
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import AuthModal from "@/Components/AuthModal";
+import UploadModal from "@/Components/UploadModal";
+
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
+
+    return ( 
+        <>
+            <AuthModal />
+            <UploadModal />
+        </>
+    );
+}
+
+export default ModalProvider;
