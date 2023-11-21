ae0f553bb0454cc4a5714cfe9c37a114134b69a3
Kevin <kevinlee.3191@gmail.com>
2023-11-20 20:07:22 -0800

    removed content and theme

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
-  content: [
-    "./src/**/*.{html,js,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
   }
 }
